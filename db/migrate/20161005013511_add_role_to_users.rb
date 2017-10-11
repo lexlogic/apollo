886396b875248a877b957ae664f0bcd99ff9175f
Nicholas Knight <nickmc01@gmail.com>
2017-10-11 15:15:12 -0400

    Fixed User Issue

--- db/migrate/20161005013511_add_role_to_users.rb
+++ db/migrate/20161005013511_add_role_to_users.rb
@@ -4,13 +4,13 @@ class AddRoleToUsers < ActiveRecord::Migration[4.2]
     add_column :users, :username, :string
     add_column :users, :full_name, :string
     
-    User.create! do |u|
-        u.username     = 'nickmc01'
-        u.full_name     = 'Nicholas Knight'
-        u.email     = 'nickmc01@gmail.com'
-        u.password    = 'Knight!@34'
-        u.password_confirmation = 'Knight!@34'
-        u.role = 'admin'
-    end
+    # User.create! do |u|
+    #     u.username     = 'nickmc01'
+    #     u.full_name     = 'Nicholas Knight'
+    #     u.email     = 'nickmc01@gmail.com'
+    #     u.password    = 'Knight!@34'
+    #     u.password_confirmation = 'Knight!@34'
+    #     u.role = 'admin'
+    # end
   end
 end
